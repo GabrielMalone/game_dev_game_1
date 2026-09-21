cef83a975e2501e31a6687cfd6186bbc4b4d8bdc
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2026-09-20 19:57:58 -0500

    tilemap stuff

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -10,7 +10,38 @@ public class TileController : MonoBehaviour
 
     void Start()
     {
-        // Turn OFF the tile at grid position (2, 3)
-        tilemap.SetTile(new Vector3Int(2, 3, 0), null);
+        
     }
+
+    void fillTileMap()
+    {
+        BoundsInt bounds = tilemap.cellBounds;
+
+        int minX = bounds.xMin;
+        int maxX = bounds.xMax;
+
+        int minY = bounds.yMin;
+        int maxY = bounds.yMax;
+
+        Debug.Log("minX: " + minX);
+        Debug.Log("maxX: " + maxX);
+        Debug.Log("minY: " + minY);
+        Debug.Log("maxY: " + maxY);
+
+
+        for (int x = bounds.xMin; x < bounds.xMax; x++)
+        {
+            for (int y = bounds.yMin; y < bounds.yMax; y++)
+            {
+                Vector3Int position = new Vector3Int(x, y, 0);
+
+                if (tilemap.HasTile(position))
+                {
+                    tilemap.SetTileFlags(position, TileFlags.None);
+                    tilemap.SetColor(position, Color.red);
+                }
+            }
+        }
+    }
+
 }
