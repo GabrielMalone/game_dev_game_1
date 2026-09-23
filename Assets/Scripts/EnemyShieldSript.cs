d424f09fc00c3958b2f9f86a7feedd8f26ceeae6
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2026-09-23 11:43:38 -0500

    can see enemy health now

--- Assets/Scripts/EnemyShieldSript.cs
+++ Assets/Scripts/EnemyShieldSript.cs
@@ -8,11 +8,16 @@ public class EnemyShieldSript : MonoBehaviour
     public int shieldSegments = 100;
     public float shieldRadius = 2f;
     Rigidbody2D rb;
+    EnemyStats enemyStats;
+    SpriteRenderer parentRenderer;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        parentRenderer = GetComponentInParent<SpriteRenderer>();
+        enemyStats = GetComponentInParent<EnemyStats>();
     }
 
     // Update is called once per frame
@@ -25,17 +30,14 @@ public class EnemyShieldSript : MonoBehaviour
     {
         shieldLine.enabled = true;
         shieldLine.useWorldSpace = true;
-        shieldLine.loop = true;
-        shieldLine.positionCount = shieldSegments;
+        shieldLine.loop = false;
+        shieldLine.positionCount = (int)enemyStats.hitPoints;
+        int cnt = (int)enemyStats.hitPoints;
 
         Vector3 center = rb.transform.position;
-
-
-
-        SpriteRenderer parentRenderer =GetComponentInParent<SpriteRenderer>();
         Color curColor = parentRenderer.material.color;
 
-        for (int i = 0; i < shieldSegments; i++)
+        for (int i = 0; i < cnt; i++)
         {   
             // 2pi / seg == how much angle between each point
             // i tells us which point currently calculating
